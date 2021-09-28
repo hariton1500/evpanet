0aa31257cb5d3b72af437779f9a45b0ca3647a97
hariton1500 <hariton1500@gmail.com>
2021-09-28 12:14:04 +0300

    main done

---
+++ lib/Screens/AuthorizationScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AuthorizationScreen extends StatefulWidget {
+  const AuthorizationScreen({ Key? key }) : super(key: key);
+
+  @override
+  _AuthorizationScreenState createState() => _AuthorizationScreenState();
+}
+
+class _AuthorizationScreenState extends State<AuthorizationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/Screens/StartScreen.dart
@@ -0,0 +1,112 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'AuthorizationScreen.dart';
+import 'MainScreen/MainScreen.dart';
+
+class StartScreen extends StatefulWidget {
+  const StartScreen({ Key? key }) : super(key: key);
+
+  @override
+  _StartScreenState createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen> {
+
+  late bool isAuthorised;
+
+  @override
+  void initState() {
+    // проверка на флаг авторизованности
+    loadShared();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Color(0xff11273c), Color(0xff3c5d7c)]
+              )
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 3,
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        child: Padding(
+                          padding: EdgeInsets.all(24.0),
+                          child: Image.asset('assets/images/splash_logo.png'),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: 20.0,
+                        left: 24.0,
+                        right: 24.0,
+                        bottom: 16.0
+                      ),
+                      child: LinearProgressIndicator(
+                        value: 1,
+                        backgroundColor: Color(0xff3c5d7c),
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                    Text(
+                      'Загрузка...',
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                        color: Colors.white
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> loadShared() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    isAuthorised = preferences.getBool('authorised') ?? false;
+    if (!isAuthorised) {
+      Timer(
+        Duration(seconds: 2),
+        await Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => AuthorizationScreen()))
+      );
+    } else {
+      Timer(
+        Duration(seconds: 2),
+        await Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => MainScreen()))
+      );
+    }
+  }
+}
