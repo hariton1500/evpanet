013b3ded36d6f33c7d449e76a78ce8bbf41734a0
hariton1500 <hariton1500@gmail.com>
2021-10-11 12:07:12 +0300

    12

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'Screens/StartScreen.dart';
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
-  //await Firebase.initializeApp();
+  await Firebase.initializeApp();
   print('Handling a background message ${message.data}');
   SharedPreferences preferences = await SharedPreferences.getInstance();
   List<String> messagesJSON = [];
