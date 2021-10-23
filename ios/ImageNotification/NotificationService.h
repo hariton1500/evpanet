9e1317b5780e17400055fa990a2f27f47f13d338
hariton1500 <hariton1500@gmail.com>
2021-10-23 12:10:35 +0300

    18

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Харитон on 22.10.2021.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
