5ede56efd1a46561fbb1345ae00858106db9d8ef
hariton1500 <hariton1500@gmail.com>
2021-11-28 11:41:35 +0300

    304_1

--- lib/Helpers/maindata.dart
+++ lib/Helpers/maindata.dart
@@ -486,3 +486,99 @@ class Abonent {
     return _response.body;
   }
 }
+
+
+
+
+//все вспомогательные классы с их методами здесь
+
+/*
+* Описание принципов работы нового API.
+*
+* Основной адрес нового API: https://evpanet.com/api/apk/
+* ***************************************************************
+* Авторизация:
+*   URL: https://evpanet.com/api/apk/login/user
+*   Method: POST
+*   Header:
+*     - key = token
+*     - value = токен от гугла
+*   Body:
+*     - number = номер телефона в формате +7....
+*     - uid = ID абонента
+*   Response:
+*     - формат: JSON
+*     - данные: массив GUID
+* ***************************************************************
+* Получение данных абонента:
+*   URL: https://evpanet.com/api/apk/user/info/<GUID>
+*   Method: GET
+*   Header:
+*     - key = token
+*     - value = токен от гугла
+*   Response:
+*     - формат: JSON
+*     - данные: данные об абоненте, и доступные тарифные планы
+* ***************************************************************
+* Изменение флагов автоактивации и родительского контроля:
+*   URLS:
+*     - https://evpanet.com/api/apk/user/parent_control/  для родительского контроля
+*     - https://evpanet.com/api/apk/user/auto_activation/ для автоактивации
+*   Method: PUT
+*   Header:
+*     - key = token
+*     - value = токен от гугла
+*   Body:
+*     - формат = JSON {"guid":"<GUID>"}
+*   Response:
+*     - формат: JSON
+*     - данные: текущее состояние флага (1 или 0)
+*
+* ***************************************************************
+* Добавление ремонта или коментария к ремонту
+*   URL: https://evpanet.com/api/apk/support/request
+*   Method: POST
+*   Header:
+*     - key = token
+*     - value = токен от гугла
+*   Body:
+*     - message = сообщение от абонента
+*     - guid
+*   Response:
+*     - формат: JSON
+*     - ответ: есть ли ошибка и текст, или сообщения или ошибки
+**
+* ***************************************************************
+* Изменение пакета или активация нового
+*   URL: https://evpanet.com/api/apk/user/tarif
+*   Method: PATCH
+*   Header:
+*     - key = token
+*     - value = токен от гугла
+*   Body:
+*     - формат = JSON {"tarif":"<tarifid>","guid":"<GUID>"}
+*   Response:
+*     - формат: JSON
+*     - данные:
+*         "packet_secs",
+*         "tarif_id",
+*         "tarif_sum",
+*         "tarif_name"
+* ***************************************************************
+* Добавление дней
+*   URL: https://evpanet.com/api/apk/user/days/
+*   Method: PUT
+*   Header:
+*     - key = token
+*     - value = токен от гугла
+*   Body:
+*     - формат = JSON {"guid":"<GUID>","days":<DAYS>}
+*   Response:
+*     - формат: JSON
+*     - данные:
+*         "days": <days>,
+*         "packet_secs": <packet_secs>,
+*         "extra_account": <extra_account>
+*
+* ***************************************************************
+* */
